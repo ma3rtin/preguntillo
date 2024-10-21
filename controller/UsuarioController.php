507ef23a94a60d82a60544f2432e38d505423387
Lira <ailinvara@gmail.com>
2024-10-21 19:26:20 -0300

    Agrega css y modifica rutas del UsuarioController.php

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -15,16 +15,19 @@ class UsuarioController
 
     public function loginForm()
     {
-        $this->presenter->show('login', []);
+        $data['css'] = '/public/css/loginForm.css';
+        $this->presenter->show('login', $data);
     }
 
     public function login()
     {
+        $data['css'] = '/public/css/loginForm.css';
         $username = isset($_POST['username']) ? $_POST['username'] : null;
         $pass = isset($_POST['pass']) ? $_POST['pass'] : null;
 
-        if (is_null($username) || is_null($pass)) {
-            $this->presenter->show('login', ["error" => "Todos los campos son obligatorios"]);
+        if (empty($username) || empty($pass)) {
+            $data['error'] = "Todos los campos son obligatorios";
+            $this->presenter->show('login', $data);
             return;
         }
         $this->model->validate($username, $pass) ?
@@ -35,7 +38,8 @@ class UsuarioController
 
     public function registerForm()
     {
-        $this->presenter->show('register', []);
+        $data['css'] = '/public/css/registerForm.css';
+        $this->presenter->show('register', $data);
     }
 
     public function register()
@@ -44,15 +48,15 @@ class UsuarioController
         $name = isset($_POST['name']) ? $_POST['name'] : null;
         $email = isset($_POST['email']) ? $_POST['email'] : null;
         $pass = isset($_POST['pass']) ? $_POST['pass'] : null;
-        $birtihyear = isset($_POST['birthyear']) ? $_POST['birthyear'] : null;
+        $birthyear = isset($_POST['birthyear']) ? $_POST['birthyear'] : null;
         $photo = ($_FILES['photo']['size'] > 0) ? $_FILES['photo'] : null;
 
-        if (is_null($user) || is_null($name) || is_null($email) || is_null($pass) || is_null($birtihyear)
+        if (is_null($user) || is_null($name) || is_null($email) || is_null($pass) || is_null($birthyear)
          || is_null($photo)) {
             $this->presenter->show('register', ["error" => "Todos los campos son obligatorios"]);
             return;
         }
-        $this->model->register($user, $name, $email, $pass, $birtihyear, $photo);
+        $this->model->register($user, $name, $email, $pass, $birthyear, $photo);
         header("location: /usuario/login");
     }
 
