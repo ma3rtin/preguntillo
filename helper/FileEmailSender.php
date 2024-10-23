032bc871093a8ad78dd6bdb69504fe79d0b054ad
maertin <mutuverria00gmail.com>
2024-10-22 21:35:27 -0300

    Agregar PHPMailer en el vendor, validación de correo electronico en el registro de usuario mediante un mail y validar que el correo esté activo al iniciar sesión

--- helper/FileEmailSender.php
+++ helper/FileEmailSender.php
@@ -1,11 +1,45 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 class FileEmailSender
 {
-    public function validateMail($to, $subject, $message)
+    private $mail;
+    public function __construct()
     {
-        $message = wordwrap($message, 70);
-        $headers = "From: email@ejemplo.com\r\n";
-        mail($to, $subject, $message, $headers);
+        $this->mail = new PHPMailer(true);
+
+        $this->mail->isSMTP();
+        $this->mail->Host = 'smtp.gmail.com';
+        $this->mail->SMTPAuth = true;
+        $this->mail->SMTPSecure = 'tls';
+        $this->mail->Port = 587;
+        $this->setCredentials();
+    }
+
+
+    public function sendValidationMail($email, $id, $token)
+    {
+        try {
+            $this->mail->addAddress($email); // Destinatario
+
+            $this->mail->Subject = "Registro exitoso";
+            $this->mail->Body = "<p>Valida tu email <a href='http://localhost/usuario/validateEmail?id=" . $id . "&token=" . $token . "'>acá</a></p>";
+            $this->mail->isHTML(true);
+
+            $this->mail->send();
+        }
+        catch (Exception $e){
+            error_log('Se produjo un error, vuelva a intentarlo'.$e->getMessage());
+        }
+    }
+
+    public function setCredentials()
+    {
+        $this->mail->setFrom('', '');
+        $this->mail->Username = "";
+        $this->mail->Password = "";
     }
 }
