02482079e66b9009a4663bf34ceaa4a9e405b37f
MateoColace <166334360+MateoColace@users.noreply.github.com>
2024-10-29 01:45:47 -0300

    -Agrugué partidaView.mustache pero html implentado -Agruegué mas lógica en el método generarPartida(). Ahora devuelve la partida que ya inserto en el db. -Generé un metodo llamado obtenerPregunta() que obtiene una pregunta de manera aleatoria y lo devuelve. -Método getPartidaConSusPreguntas(), esta a medio hacerm pero llamaría generarPartida() y obtenerPregunta() para obtener los valores y hacer insert into a pregunta_partida con dichos valores, luego llamaría con una consulta a opcion y pregunta para devolver un array con partida,pregunta y opción.

--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -13,13 +13,40 @@ class PartidaModel{
         return $usuarios[0]['id'];
     }
 
-    public function generarPartida(){
+    private function generarPartida(){
         $id_user = $this->getIdDeUsuarioActivo();
         if($id_user != null){
-            $sql = "INSERT INTO partida(estado,puntaje,fecha,usuario_id)
+            $insert = "INSERT INTO partida(estado,puntaje,fecha,usuario_id)
                     values (1,0,CURRENT_DATE,$id_user);";
-            $this->database->execute($sql);
+            $this->database->execute($insert);
         }
+        $sql = "SELECT * FROM partida where estado = 1;";
+        $partidas = $this->database->query($sql);
+
+        return $partidas[0];
+    }
+
+    public function obtenerPregunta(){
+        $sql = "SELECT id FROM pregunta where estado = 1 order by rand() limit 1;";
+        $preguntas = $this->database->query($sql);
+
+        return $preguntas[0]['id'];
+    }
+
+    public function getPartidaConSusPreguntas(){
+        $partida = $this->generarPartida();
+        $insert = "INSERT INTO partida_pregunta(partida_id,pregunta_id)
+                   values (".$partida['id'].",".$this->obtenerPregunta().");";
+        $this->database->execute($insert);
+
+        $sql = "select pr.descripcion, op.descripcion 
+                from pregunta pr 
+                join partida_pregunta parp on pr.id = parp.pregunta_id
+                join partida part on part.id = parp.partida_id
+                join pregunta_opcion prop on prop.pregunta_id = parp.pregunta_id
+                join opcion op on op.id = prop.opcion_incorrecta
+                where op.id = pr.opcion_correcta;";
 
     }
+
 }
